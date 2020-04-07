da279b7de994ee04ba77a977652e9f2c2d30d7a7
Raka <mgf.prauliyatama@gmail.com>
2020-04-07 09:13:35 +0700

    feat(firebase): configuraing firebase

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,8 +1,9 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/widgets/rounded_button.dart';
 import 'package:flutter/material.dart';
 
 import 'login_screen.dart';
-import 'registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String routeName = 'welcome_screen';
@@ -73,41 +74,19 @@ class _WelcomeScreenState extends State<WelcomeScreen>
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.routeName);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              buttonText: 'Login',
+              color: Colors.lightBlueAccent,
+              onTap: () {
+                Navigator.pushNamed(context, LoginScreen.routeName);
+              },
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.routeName);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            RoundedButton(
+              buttonText: 'Register',
+              color: Colors.blueAccent,
+              onTap: () {
+                Navigator.pushNamed(context, RegistrationScreen.routeName);
+              },
             ),
           ],
         ),
